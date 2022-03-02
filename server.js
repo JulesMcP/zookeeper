24f78b9f7347755f54814052b6bb6a3134f97535
JulesMcP <rogue427@yahoo.com>
2022-03-01 21:46:49 -0600

    tried to fix my code using code snapshot, it still doesn't work

--- server.js
+++ server.js
@@ -5,17 +5,14 @@ const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
-// tells where to access files
 app.use(express.static('public'));
 
-// use apiRoutes
+// Use apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
